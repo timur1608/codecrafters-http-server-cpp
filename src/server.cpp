4a48d33ab33082b91f075abbc612129ed4caa849
timur1608 <stim20045@list.ru>
2024-06-12 01:12:33 +0300

    realized second step

--- src/server.cpp
+++ src/server.cpp
@@ -53,9 +53,11 @@ int main(int argc, char **argv) {
   
   std::cout << "Waiting for a client to connect...\n";
   
-  accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
   std::cout << "Client connected\n";
   
+  const char* buf = "HTTP/1.1 200 OK\r\n\r\n";
+  send(client_fd, buf, 23, 0);
   close(server_fd);
 
   return 0;
