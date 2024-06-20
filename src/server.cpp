e4e64d23c2e699c8ac8693e7a5ec48b2d019cb37
timur1608 <stim20045@list.ru>
2024-06-20 17:10:31 +0300

    add extracting url path

--- src/server.cpp
+++ src/server.cpp
@@ -1,63 +1,94 @@
-#include <iostream>
-#include <cstdlib>
-#include <string>
-#include <cstring>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
 #include <arpa/inet.h>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
 #include <netdb.h>
+#include <string>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+std::string getUrl(char *buffer) {
+  std::string result;
+  char* urlp = strtok(buffer, "/");
+  urlp = strtok(NULL, "/");
+  while (*urlp != ' '){
+    result += *urlp; 
+    urlp++;
+  }
+  return result;
+}
 
 int main(int argc, char **argv) {
   // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-  
-  // You can use print statements as follows for debugging, they'll be visible when running tests.
+
+  // You can use print statements as follows for debugging, they'll be visible
+  // when running tests.
   std::cout << "Logs from your program will appear here!\n";
 
-  //Uncomment this block to pass the first stage
-  
+  // Uncomment this block to pass the first stage
+
   int server_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (server_fd < 0) {
-   std::cerr << "Failed to create server socket\n";
-   return 1;
+    std::cerr << "Failed to create server socket\n";
+    return 1;
   }
-  
+
   // Since the tester restarts your program quite often, setting SO_REUSEADDR
   // ensures that we don't run into 'Address already in use' errors
   int reuse = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) < 0) {
+  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuse, sizeof(reuse)) <
+      0) {
     std::cerr << "setsockopt failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in server_addr;
   server_addr.sin_family = AF_INET;
   server_addr.sin_addr.s_addr = INADDR_ANY;
   server_addr.sin_port = htons(4221);
-  
-  if (bind(server_fd, (struct sockaddr *) &server_addr, sizeof(server_addr)) != 0) {
+
+  if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) !=
+      0) {
     std::cerr << "Failed to bind to port 4221\n";
     return 1;
   }
-  
+
   int connection_backlog = 5;
   if (listen(server_fd, connection_backlog) != 0) {
     std::cerr << "listen failed\n";
     return 1;
   }
-  
+
   struct sockaddr_in client_addr;
   int client_addr_len = sizeof(client_addr);
-  
+
   std::cout << "Waiting for a client to connect...\n";
-  
-  int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, (socklen_t *) &client_addr_len);
+
+  int client_fd = accept(server_fd, (struct sockaddr *)&client_addr,
+                         (socklen_t *)&client_addr_len);
   std::cout << "Client connected\n";
-  
-  const char* buf = "HTTP/1.1 200 OK\r\n\r\n";
-  send(client_fd, buf, 23, 0);
+
+  char buffer[512];
+  // sockaddr_in SenderAddr;
+  // unsigned int SenderAddrSize = sizeof(SenderAddr);
+
+  // auto size = recv(server_fd, &buffer[0], 512, 0);
+  bzero(buffer, 512);
+  auto size = read(client_fd, buffer, 512);
+  // std::cout << size << std::endl;
+
+  std::string OK("HTTP/1.1 200 OK\r\n\r\n");
+  std::string ERROR("HTTP/1.1 404 Not Found\r\n\r\n");
+  std::string url(getUrl(buffer));
+  std::cout << url << std::endl;
+  if (url == "index.html") {
+    send(client_fd, OK.c_str(), 23, 0);
+  } else {
+    send(client_fd, ERROR.c_str(), 30, 0);
+  }
   close(server_fd);
 
   return 0;
